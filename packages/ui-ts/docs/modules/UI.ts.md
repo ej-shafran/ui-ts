313fe59a039f36faca00073ab516e0c3cfcf6777
ej-shafran <ejshafran@hilma.tech>
2023-09-01 21:13:03 +0300

    feat: add UI module

---
+++ packages/ui-ts/docs/modules/UI.ts.md
@@ -0,0 +1,356 @@
+---
+title: UI.ts
+nav_order: 3
+parent: Modules
+---
+
+## UI overview
+
+Added in v1.0.0
+
+---
+
+<h2 class="text-delta">Table of contents</h2>
+
+- [app](#app)
+  - [App (type alias)](#app-type-alias)
+  - [Render (type alias)](#render-type-alias)
+  - [Trigger (type alias)](#trigger-type-alias)
+  - [Update (type alias)](#update-type-alias)
+  - [createApp](#createapp)
+  - [runApp](#runapp)
+  - [runWithRoot](#runwithroot)
+- [jsx](#jsx)
+  - [element](#element)
+  - [fragment](#fragment)
+- [model](#model)
+  - [Element (type alias)](#element-type-alias)
+  - [Node](#node)
+  - [TagName (type alias)](#tagname-type-alias)
+  - [UINode (type alias)](#uinode-type-alias)
+
+---
+
+# app
+
+## App (type alias)
+
+Return type of `createApp`.
+
+**Signature**
+
+```ts
+export type App = (root: HTMLElement) => IOO.IOOption<void>
+```
+
+Added in v1.0.0
+
+## Render (type alias)
+
+Type of the `render` callback of `createApp`.
+
+**Signature**
+
+```ts
+export type Render<TState, TEvent> = (trigger: Trigger<TEvent>, state: Readonly<TState>) => Element
+```
+
+Added in v1.0.0
+
+## Trigger (type alias)
+
+Type of the `trigger` param passed to the `render` callback of `createApp`.
+
+**Signature**
+
+```ts
+export type Trigger<TEvent> = (e: TEvent) => void
+```
+
+Added in v1.0.0
+
+## Update (type alias)
+
+Type of the `update` callback of `createApp`.
+
+**Signature**
+
+```ts
+export type Update<TState, TEvent> = (e: TEvent) => (state: Readonly<TState>) => TState
+```
+
+Added in v1.0.0
+
+## createApp
+
+**Signature**
+
+```ts
+export declare const createApp: <TState, TEvent>(
+  initial: TState,
+  render: Render<TState, TEvent>,
+  update: Update<TState, TEvent>
+) => App
+```
+
+**Example**
+
+```ts
+import { ADT, match } from 'ts-adt'
+import * as UI from 'ui-ts/lib/UI'
+
+type Event = ADT<{
+  Click: {}
+}>
+
+const Click: Event = { _type: 'Click' }
+
+type State = {
+  count: number
+}
+
+const initial: State = {
+  count: 0,
+}
+
+// you'll probably use JSX for this, like
+// <button onClick={() => trigger(Click)}>Count: {state.count}</button>
+const render: UI.Render<State, Event> = (trigger, state) =>
+  UI.element('button', { onClick: () => trigger(Click) }, 'Count: ', state.count)
+
+const update: UI.Update<State, Event> = match({
+  Click:
+    () =>
+    ({ count }: State) => ({ count: count + 1 }),
+})
+
+// this is an `App` that can later be run with `runApp`
+const app = UI.createApp(initial, render, update)
+```
+
+Added in v1.0.0
+
+## runApp
+
+Run an `App` with a certain HTML elemtn, given that element's ID.
+
+**Signature**
+
+```ts
+export declare const runApp: (app: App, rootId?: string | undefined) => void
+```
+
+Added in v1.0.0
+
+## runWithRoot
+
+A low-level function for running an `App`, in case you can't get the `HTMLElement` through `document.getElementById`.
+
+**Signature**
+
+```ts
+export declare const runWithRoot: (app: App, root: HTMLElement) => boolean
+```
+
+Added in v1.0.0
+
+# jsx
+
+## element
+
+**Signature**
+
+```ts
+export declare const element: (
+  tagName:
+    | 'object'
+    | 'fragment'
+    | 'a'
+    | 'abbr'
+    | 'address'
+    | 'applet'
+    | 'area'
+    | 'article'
+    | 'aside'
+    | 'audio'
+    | 'b'
+    | 'base'
+    | 'basefont'
+    | 'bdi'
+    | 'bdo'
+    | 'blockquote'
+    | 'body'
+    | 'br'
+    | 'button'
+    | 'canvas'
+    | 'caption'
+    | 'cite'
+    | 'code'
+    | 'col'
+    | 'colgroup'
+    | 'data'
+    | 'datalist'
+    | 'dd'
+    | 'del'
+    | 'details'
+    | 'dfn'
+    | 'dialog'
+    | 'dir'
+    | 'div'
+    | 'dl'
+    | 'dt'
+    | 'em'
+    | 'embed'
+    | 'fieldset'
+    | 'figcaption'
+    | 'figure'
+    | 'font'
+    | 'footer'
+    | 'form'
+    | 'frame'
+    | 'frameset'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'head'
+    | 'header'
+    | 'hgroup'
+    | 'hr'
+    | 'html'
+    | 'i'
+    | 'iframe'
+    | 'img'
+    | 'input'
+    | 'ins'
+    | 'kbd'
+    | 'label'
+    | 'legend'
+    | 'li'
+    | 'link'
+    | 'main'
+    | 'map'
+    | 'mark'
+    | 'marquee'
+    | 'menu'
+    | 'meta'
+    | 'meter'
+    | 'nav'
+    | 'noscript'
+    | 'ol'
+    | 'optgroup'
+    | 'option'
+    | 'output'
+    | 'p'
+    | 'param'
+    | 'picture'
+    | 'pre'
+    | 'progress'
+    | 'q'
+    | 'rp'
+    | 'rt'
+    | 'ruby'
+    | 's'
+    | 'samp'
+    | 'script'
+    | 'section'
+    | 'select'
+    | 'slot'
+    | 'small'
+    | 'source'
+    | 'span'
+    | 'strong'
+    | 'style'
+    | 'sub'
+    | 'summary'
+    | 'sup'
+    | 'table'
+    | 'tbody'
+    | 'td'
+    | 'template'
+    | 'textarea'
+    | 'tfoot'
+    | 'th'
+    | 'thead'
+    | 'time'
+    | 'title'
+    | 'tr'
+    | 'track'
+    | 'u'
+    | 'ul'
+    | 'var'
+    | 'video'
+    | 'wbr'
+    | ((props: Record<string, unknown>) => Element),
+  props: Record<string, unknown> | null,
+  ...children: (string | number | Element)[]
+) => Element
+```
+
+Added in v1.0.0
+
+## fragment
+
+**Signature**
+
+```ts
+export declare const fragment: 'fragment'
+```
+
+Added in v1.0.0
+
+# model
+
+## Element (type alias)
+
+**Signature**
+
+```ts
+export type Element = ADT<{
+  Root: {
+    tagName: TagName
+    props: Record<string, unknown> | null
+    children: Element[]
+  }
+  Text: {
+    value: string
+  }
+  Fragment: {
+    children: Element[]
+  }
+}>
+```
+
+Added in v1.0.0
+
+## Node
+
+**Signature**
+
+```ts
+export declare const Node: UINode
+```
+
+Added in v1.0.0
+
+## TagName (type alias)
+
+**Signature**
+
+```ts
+export type TagName = keyof HTMLElementTagNameMap
+```
+
+Added in v1.0.0
+
+## UINode (type alias)
+
+**Signature**
+
+```ts
+type UINode = string | number | boolean | Element | UINode[]
+```
+
+Added in v1.0.0
---
+++ packages/ui-ts/src/UI.ts
@@ -0,0 +1,498 @@
+/* @since 1.0.0 */
+
+import { ADT, match } from "ts-adt";
+import { pipe } from "fp-ts/function";
+import * as A from "fp-ts/Array";
+import * as IO from "fp-ts/IO";
+import * as IOO from "fp-ts/IOOption";
+import * as R from "fp-ts/Record";
+import * as O from "fp-ts/Option";
+import * as DOM from "./DOM";
+
+/**
+ * @category model
+ * @since 1.0.0
+ */
+export type TagName = keyof HTMLElementTagNameMap;
+
+/**
+ * @category model
+ * @since 1.0.0
+ */
+export type Element = ADT<{
+  Root: {
+    tagName: TagName;
+    props: Record<string, unknown> | null;
+    children: Element[];
+  };
+  Text: {
+    value: string;
+  };
+  Fragment: {
+    children: Element[];
+  };
+}>;
+
+type RootElement = Element & { _type: "Root" };
+type TextElement = Element & { _type: "Text" };
+type FragmentElement = Element & { _type: "Fragment" };
+
+/**
+ * @category model
+ * @since 1.0.0
+ */
+type UINode = string | number | boolean | Element | UINode[];
+export type {
+  /**
+   * @category model
+   * @since 1.0.0
+   */
+  UINode as Node,
+};
+
+type Instance =
+  | {
+      elem: RootElement;
+      domNode: HTMLElement;
+      childInsts: Instance[];
+    }
+  | {
+      elem: TextElement;
+      domNode: Text;
+    }
+  | {
+      elem: FragmentElement;
+      domNode: DocumentFragment;
+      childInsts: Instance[];
+    };
+
+const normalizeChildren = (children: UINode[]): Element[] =>
+  pipe(
+    children,
+    A.flatMap((child) => {
+      switch (typeof child) {
+        case "object": {
+          if (!child) return [];
+
+          if (Array.isArray(child)) return normalizeChildren(child);
+
+          return [child];
+        }
+        case "boolean":
+        case "undefined":
+          return [];
+        case "string":
+        case "number":
+        case "bigint":
+        case "symbol":
+          return [
+            {
+              _type: "Text",
+              value: String(child),
+            },
+          ];
+      }
+    }),
+  );
+
+/**
+ * @category jsx
+ * @since 1.0.0
+ */
+export const fragment = "fragment";
+
+/**
+ * @category jsx
+ * @since 1.0.0
+ */
+export const element = (
+  tagName:
+    | keyof HTMLElementTagNameMap
+    | typeof fragment
+    | ((props: Record<string, unknown>) => Element),
+  props: Record<string, unknown> | null,
+  ...children: (string | number | Element)[]
+): Element => {
+  if (tagName === fragment) {
+    return { _type: "Fragment", children: normalizeChildren(children) };
+  } else if (typeof tagName === "string") {
+    return {
+      _type: "Root",
+      tagName,
+      props,
+      children: normalizeChildren(children),
+    };
+  } else if (typeof tagName === "function") {
+    return tagName(props ?? {});
+  } else {
+    throw new Error(`Cannot construct UI.Element from ${String(tagName)}`);
+  }
+};
+
+type PropertyCallback<A = unknown> = (
+  k: string,
+  a: A,
+) => (element: HTMLElement) => IO.IO<void>;
+
+const matchProp: (
+  listenerCb: PropertyCallback<EventListener>,
+  propCb: PropertyCallback<string>,
+) => PropertyCallback = (listenerCb, propCb) => (k, a) => (element) => {
+  if (k.startsWith("on") && a instanceof Function) {
+    return pipe(
+      element,
+      listenerCb(k.slice(2).toLowerCase(), a as EventListener),
+    );
+  } else {
+    return pipe(element, propCb(k, String(a)));
+  }
+};
+
+const removeProp: PropertyCallback = matchProp(
+  DOM.removeEventListener,
+  DOM.removeAttribute,
+);
+
+const addProp: PropertyCallback = matchProp(
+  DOM.addEventListener,
+  DOM.setAttribute,
+);
+
+const updateDOM =
+  (prev: Record<string, unknown>, next: Record<string, unknown>) =>
+  (domNode: HTMLElement): IO.IO<HTMLElement> => {
+    return pipe(
+      prev,
+      R.mapWithIndex((k, a) => pipe(domNode, removeProp(k, a))),
+      () => next,
+      R.mapWithIndex((k, a) => pipe(domNode, addProp(k, a))),
+      () => () => domNode,
+    );
+  };
+
+const hasOnInsert = (
+  props: object | null,
+): props is { onInsert: (domNode: HTMLElement) => void } =>
+  !!props && "onInsert" in props && props["onInsert"] instanceof Function;
+
+const createInstance: (elem: Element) => IO.IO<Instance> = (elem) => {
+  return pipe(
+    elem,
+    match({
+      Root: (elem) =>
+        pipe(
+          IO.Do,
+          IO.bind("domNode", () => DOM.createElement(elem.tagName)),
+          IO.tap(({ domNode }) =>
+            pipe(domNode, updateDOM({}, elem.props ?? {})),
+          ),
+          IO.bind(
+            "childInsts",
+            () =>
+              IO.sequenceArray(elem.children.map(createInstance)) as IO.IO<
+                Instance[]
+              >,
+          ),
+          IO.tap(
+            ({ childInsts, domNode }) =>
+              () =>
+                childInsts.map((child) =>
+                  DOM.appendChild(child.domNode)(domNode),
+                ),
+          ),
+          IO.tap(({ domNode }) => () => {
+            if (hasOnInsert(elem.props)) {
+              requestIdleCallback(() => {
+                if (hasOnInsert(elem.props)) {
+                  return elem.props["onInsert"](domNode);
+                }
+              });
+            }
+          }),
+          IO.bind("elem", () => () => elem),
+        ),
+      Text: (elem) =>
+        pipe(
+          elem.value,
+          DOM.createTextNode,
+          IO.map((domNode) => ({ domNode, elem })),
+        ),
+      Fragment: (elem) =>
+        pipe(
+          IO.Do,
+          IO.bind("domNode", () => DOM.createDocumentFragment()),
+          IO.bind(
+            "childInsts",
+            () =>
+              IO.sequenceArray(elem.children.map(createInstance)) as IO.IO<
+                Instance[]
+              >,
+          ),
+          IO.tap(
+            ({ childInsts, domNode }) =>
+              () =>
+                childInsts.map((child) =>
+                  DOM.appendChild(child.domNode)(domNode),
+                ),
+          ),
+          IO.map(({ domNode, childInsts }) => ({ domNode, elem, childInsts })),
+        ),
+    }),
+  );
+};
+
+const reconcileAdd: (container: Node) => (elem: Element) => IO.IO<Instance> =
+  (container) => (elem) =>
+    pipe(
+      IO.Do,
+      IO.bind("inst", () => createInstance(elem)),
+      IO.tap(({ inst }) => pipe(container, DOM.appendChild(inst.domNode))),
+      IO.map(({ inst }) => inst),
+    );
+
+const reconcileRemove: (
+  container: Node,
+) => (inst: Instance) => IOO.IOOption<Instance> = (container) => (inst) =>
+  pipe(
+    container,
+    DOM.removeChild(inst.domNode),
+    IOO.flatMap(() => IOO.none),
+  );
+
+const reconcileReplace: (
+  container: Node,
+  elem: Element,
+  inst: Instance,
+) => IOO.IOOption<Instance> = (container, elem, inst) =>
+  pipe(
+    createInstance(elem),
+    IOO.fromIO,
+    IOO.tap((newInst) =>
+      DOM.replaceChild(newInst.domNode, inst.domNode)(container),
+    ),
+  );
+
+const reconcileChildren = (
+  elem: RootElement | FragmentElement,
+  inst: Instance,
+) => {
+  const maxLength = Math.max(
+    (inst as Instance & { childInsts: Instance[] }).childInsts.length,
+    elem.children.length,
+  );
+
+  const childInsts = [] as Instance[];
+  for (let i = 0; i < maxLength; i++) {
+    const childElem = O.fromNullable(elem.children[i]);
+    const childInst = O.fromNullable(
+      (inst as Instance & { childInsts: Instance[] }).childInsts[i],
+    );
+
+    const childIO = reconcile(inst.domNode, childElem, childInst);
+
+    pipe(
+      childIO(),
+      O.map((inst) => childInsts.push(inst)),
+    );
+  }
+
+  return childInsts;
+};
+
+const reconcileUpdate: (elem: Element, inst: Instance) => IO.IO<Instance> = (
+  elem,
+  inst,
+) =>
+  pipe(
+    elem,
+    match({
+      Root: (elem) => {
+        pipe(
+          inst.domNode as HTMLElement,
+          updateDOM((inst.elem as RootElement).props ?? {}, elem.props ?? {}),
+        );
+
+        inst.elem = elem;
+
+        const childInsts = reconcileChildren(elem, inst);
+
+        return () => ({
+          elem,
+          childInsts,
+          domNode: inst.domNode as HTMLElement,
+        });
+      },
+      Text: (elem) => {
+        if (elem.value !== (inst.elem as TextElement).value) {
+          inst.domNode.nodeValue = elem.value;
+          inst.elem = elem;
+        }
+        return () => inst;
+      },
+      Fragment: (elem) => {
+        const childInsts = reconcileChildren(elem, inst);
+
+        (inst as Instance & { childInsts: Instance[] }).childInsts = childInsts;
+
+        return () => inst;
+      },
+    }),
+  );
+
+const reconcile: (
+  container: Node,
+  elem: O.Option<Element>,
+  inst: O.Option<Instance>,
+) => IOO.IOOption<Instance> = (container, elem, inst) => {
+  if (O.isNone(inst)) {
+    return pipe(elem, IOO.fromOption, IOO.flatMapIO(reconcileAdd(container)));
+  } else if (O.isNone(elem)) {
+    return pipe(inst, IOO.fromOption, IOO.flatMap(reconcileRemove(container)));
+  } else if (
+    elem.value._type !== inst.value.elem._type ||
+    (elem.value as RootElement).tagName !==
+      (inst.value.elem as RootElement).tagName
+  ) {
+    return reconcileReplace(container, elem.value, inst.value);
+  } else {
+    return pipe(reconcileUpdate(elem.value, inst.value), IOO.fromIO);
+  }
+};
+
+/**
+ * Type of the `trigger` param passed to the `render` callback of `createApp`.
+ *
+ * @category app
+ * @since 1.0.0
+ */
+export type Trigger<TEvent> = (e: TEvent) => void;
+/**
+ * Type of the `render` callback of `createApp`.
+ *
+ * @category app
+ * @since 1.0.0
+ */
+export type Render<TState, TEvent> = (
+  trigger: Trigger<TEvent>,
+  state: Readonly<TState>,
+) => Element;
+
+/**
+ * Type of the `update` callback of `createApp`.
+ *
+ * @category app
+ * @since 1.0.0
+ */
+export type Update<TState, TEvent> = (
+  e: TEvent,
+) => (state: Readonly<TState>) => TState;
+
+/**
+ * Return type of `createApp`.
+ *
+ * @category app
+ * @since 1.0.0
+ */
+export type App = (root: HTMLElement) => IOO.IOOption<void>;
+
+/**
+ * @example
+ * import { ADT, match } from 'ts-adt';
+ * import * as UI from 'ui-ts/lib/UI';
+ *
+ * type Event = ADT<{
+ *   Click: {};
+ * }>;
+ *
+ * const Click: Event = { _type: "Click" };
+ *
+ * type State = {
+ *   count: number;
+ * };
+ *
+ * const initial: State = {
+ *   count: 0,
+ * };
+ *
+ * // you'll probably use JSX for this, like
+ * // <button onClick={() => trigger(Click)}>Count: {state.count}</button>
+ * const render: UI.Render<State, Event> = (trigger, state) =>
+ *    UI.element("button", { onClick: () => trigger(Click) }, "Count: ", state.count);
+ *
+ * const update: UI.Update<State, Event> = match({
+ *   Click: () => ({ count }: State) => ({ count: count + 1 }),
+ * });
+ *
+ * // this is an `App` that can later be run with `runApp`
+ * const app = UI.createApp(initial, render, update);
+ *
+ * @category app
+ * @since 1.0.0
+ */
+export const createApp =
+  <TState, TEvent>(
+    initial: TState,
+    render: Render<TState, TEvent>,
+    update: Update<TState, TEvent>,
+  ): App =>
+  (root) => {
+    return pipe(
+      IOO.some(root),
+      IOO.map((container) => {
+        let state = initial;
+        let rootInst: O.Option<Instance> = O.none;
+        let rootElem: O.Option<Element> = O.none;
+
+        function trigger(e: TEvent) {
+          state = update(e)(state);
+          rootElem = O.some(render(trigger, state));
+          rootInst = reconcile(container, rootElem, rootInst)();
+        }
+
+        rootElem = O.some(render(trigger, state));
+        rootInst = reconcile(container, rootElem, rootInst)();
+      }),
+    );
+  };
+
+/**
+ * A low-level function for running an `App`, in case you can't get the `HTMLElement` through `document.getElementById`.
+ *
+ * @category app
+ * @since 1.0.0
+ */
+export const runWithRoot: (app: App, root: HTMLElement) => boolean = (
+  app,
+  root,
+) => {
+  const runnable = app(root);
+  const result = runnable();
+  return O.isSome(result);
+};
+
+/**
+ * Run an `App` with a certain HTML elemtn, given that element's ID.
+ *
+ * @param app The `App` to run
+ * @param rootId The ID of the element to append the resulting HTML to. Default: `"root"`.
+ *
+ * @category app
+ * @since 1.0.0
+ */
+export const runApp: (app: App, rootId?: string) => void = (
+  app,
+  rootId = "root",
+) => {
+  const runnable = pipe(
+    rootId,
+    DOM.getElementById(document),
+    IOO.map((root) => runWithRoot(app, root)),
+  );
+  const result = runnable();
+
+  if (O.isNone(result) || result.value === false) {
+    throw new Error(
+      "Something went wrong when running the application. Time to debug :)",
+    );
+  }
+};
